cade569c2848c62264ca69c6f9943dd6412e5396
zxanderrodriguez <zxanderrodriguez@gmail.com>
2021-08-30 19:27:59 -0500

    first

---
+++ Assets/scripts/CharacterScripts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScripts : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y, rb.velocity.z);
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, Input.GetAxisRaw("Vertical") * moveSpeed);
+    }
+}
---
+++ Assets/scripts/InteractableText.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Canvas theCanvaas;
+    public Text displayText;
+    public string objectsText;
+    private bool isActive;
+    private bool playerInArea = false;
+    void Start()
+    {
+        theCanvaas.gameObject.SetActive(false);
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown("q"))
+        {
+            if (!isActive) { 
+                
+                    Debug.Log("open");
+                    theCanvaas.gameObject.SetActive(true);
+                    displayText.text = objectsText;
+                    isActive = true;
+                
+
+            }
+            else
+            {
+               
+                    Debug.Log("close");
+                    theCanvaas.gameObject.SetActive(false);
+                    displayText.text = "";
+                    isActive = false;
+                
+            }
+
+        }
+
+    }
+}
+
+
+
